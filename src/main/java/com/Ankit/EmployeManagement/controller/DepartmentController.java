ab88833b36bfbaf9302eeb0a8d0e241614074f57
Ankit399648 <ap806455@gmail.com>
2026-06-16 23:58:29 +0530

    working on the controller

--- src/main/java/com/Ankit/EmployeManagement/controller/DepartmentController.java
+++ src/main/java/com/Ankit/EmployeManagement/controller/DepartmentController.java
@@ -3,11 +3,11 @@ package com.Ankit.EmployeManagement.controller;
 import com.Ankit.EmployeManagement.dto.response.DepartmentResponseDto;
 import com.Ankit.EmployeManagement.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,7 +16,18 @@ public class DepartmentController {
     private final DepartmentService departmentService;
 
     @GetMapping("/")
-    public List<DepartmentResponseDto> getAllDepartements() {
-        return departmentService.getAllDepartments();
+    public ResponseEntity<Page<DepartmentResponseDto>> getAllDepartements(@RequestParam int page, @RequestParam int size) {
+        Page<DepartmentResponseDto> response = departmentService.getAllDepartments(page, size);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DepartmentResponseDto> getDepartementById(@PathVariable long id) {
+        return ResponseEntity.ok().body(departmentService.getDepartmentById(id));
+    }
+
+    @GetMapping("/name/{name}")
+    public DepartmentResponseDto getDepartmentByName(@PathVariable String name) {
+        return departmentService.getDepartmentByName(name);
     }
 }
